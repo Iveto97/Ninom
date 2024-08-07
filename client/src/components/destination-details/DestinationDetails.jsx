895ebf72352c6bce51686e03dc311b8bafcf9192
Iva <141308984+Iveto97@users.noreply.github.com>
2024-08-07 19:50:56 +0300

    Add user functionality

--- client/src/components/destination-details/DestinationDetails.jsx
+++ client/src/components/destination-details/DestinationDetails.jsx
@@ -24,7 +24,7 @@ export default function DestinationDetails() {
   const { email, userId, isAuthenticated } = useAuthContext();
 
   const [ destination ] = useGetOneDestination(destinationId)
-  
+    
   const commentCreator = async ({ comment }) => {
     try {
       const newComment = await createComment(destinationId, comment);
@@ -54,79 +54,81 @@ export default function DestinationDetails() {
     setCurrentIndex(newIndex >= length ? 0 : newIndex);
   };
   
+
+  const isCreator = userId === destination._ownerId;
   return (
     <section id="game-details">
       <div className={styles["info-section"]}>
         <div className={styles["game-header"]}>
-      {/* <h1>{destination.title}</h1> */}
-      <p>{destination.details}</p>
+          {/* <h1>{destination.title}</h1> */}
+          <p>{destination.details}</p>
 
-      <div className={styles["slideshow-container"]}>
-        <div className={styles[("mySlides", "fade")]}>
-          <div className={styles["numbertext"]}>
-            {currentIndex + 1} / {length}
+          <div className={styles["slideshow-container"]}>
+            <div className={styles[("mySlides", "fade")]}>
+              <div className={styles["numbertext"]}>
+                {currentIndex + 1} / {length}
+              </div>
+              <img
+                src={
+                  destination.img && destination.img.length > 0
+                    ? destination.img[currentIndex]
+                    : ""
+                }
+                style={{ width: 1000 }}
+              />
+            </div>
+            <a className={styles["prev"]} onClick={clickHandlePrev}>
+              &#10094;
+            </a>
+            <a className={styles["next"]} onClick={clickHandleNext}>
+              &#10095;
+            </a>
           </div>
-          <img src={destination.img && destination.img.length > 0 ? destination.img[currentIndex] : ''} style={{ width: 1000 }} />
+         {isCreator &&  (<div className={styles["buttons"]}>
+            <a href="#" className={styles["buttons"]}>
+              Edit
+            </a>
+            <a href="#" className={styles["buttons"]}>
+              Delete
+            </a>
+          </div>)}
         </div>
-        <a className={styles["prev"]} onClick={clickHandlePrev}>
-          &#10094;
-        </a>
-        <a className={styles["next"]} onClick={clickHandleNext}>
-          &#10095;
-        </a>
       </div>
-      <div className={styles["buttons"]}>
-          <a href="#" className={styles["buttons"]}>
-            Edit
-          </a>
-          <a href="#" className={styles["buttons"]}>
-            Delete
-          </a>
-        </div>
-    </div>
-        
-        </div>
-       
-        <div className={styles["details-comments"]}>
-          <h2>Comments:</h2>
-          <ul>
-            {
-              comments.map(comment => (
-                <li key={comment._id} className={styles["comment"]}>
-                  <p>
-                    {comment._ownerId}: {comment.text}
-                  </p>
-                </li>
-              ))}
-          </ul>
-          {comments.length === 0 &&
-            <p className={styles["no-comment"]}>No comments.</p>
-         }
-        </div>
 
-      <article className={styles["create-comment"]}>
-        <label>Коментар:</label>
-        <form className={styles["form"]} >
-          <input
-            type="text"
-            placeholder="Pesho"
-            name="username"
-            // onChange={(e) => setUsername(e.target.value)}
-            // value={username}
-          />
-          <textarea
-            name="comment"
-            placeholder="Comment......"
-            // onChange={(e) => setComment(e.target.value)}
-            // value={comment}
-          ></textarea>
-          <input
-            className={styles[("btn", "submit")]}
-            type="submit"
-            value="Add Comment"
-          />
-        </form>
-      </article>
+      <div className={styles["details-comments"]}>
+        <h2>Comments:</h2>
+        <ul>
+          {comments.map((comment) => (
+            <li key={comment._id} className={styles["comment"]}>
+              <p>
+                {comment._ownerId}: {comment.text}
+              </p>
+            </li>
+          ))}
+        </ul>
+        {comments.length === 0 && (
+          <p className={styles["no-comment"]}>No comments.</p>
+        )}
+      </div>
+
+      {isAuthenticated && (
+        <article className={styles["create-comment"]}>
+          <label>Коментар:</label>
+          <form className={styles["form"]} onSubmit={submitHandler}>
+            <textarea
+              name="comment"
+              placeholder="Comment......"
+              onChange={changeHandler}
+              value={values.comment}
+            ></textarea>
+            <input
+              className={styles[("btn", "submit")]}
+              type="submit"
+              value="Add Comment"
+            />
+          </form>
+        </article>
+      )}
     </section>
   );
 }
