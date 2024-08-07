71fbf4f33657d867f48520bd3ca6056e4d69cb48
Iva <141308984+Iveto97@users.noreply.github.com>
2024-08-07 20:31:26 +0300

    Update logout implementation

--- client/src/components/logout/Logout.jsx
+++ client/src/components/logout/Logout.jsx
@@ -1,24 +1,11 @@
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useLogout } from "../../hooks/useAuth";
 import useForm from "../../hooks/useForm";
 
 export default function Logout() {
     const logout = useLogout();
-    const navigate = useNavigate();
 
-    const initialValues = { email: '', psw: '' };
-
-    const logoutHandler = async () => {
-        try {
+   
             logout();
-
-                navigate('/');
-            
-        } catch (error) {
-            console.log(error.message);
-        };
-    };
-logoutHandler();
-
-    // const { } = useForm( initialValues, logoutHandler );
+        return <Navigate to={'/'} />
 };
