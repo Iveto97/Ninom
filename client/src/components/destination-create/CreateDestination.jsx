2fabe2d940bb946265f6a90943ab4ad05cf635e7
Iva <141308984+Iveto97@users.noreply.github.com>
2024-08-07 22:11:32 +0300

    Refactoring create destination

--- client/src/components/destination-create/CreateDestination.jsx
+++ client/src/components/destination-create/CreateDestination.jsx
@@ -1,60 +1,82 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import styles from './CreateDestination.module.css';
+import useForm from "../../hooks/useForm";
+import { createDest } from "../../api/dest-api";
 
-const isUser = localStorage.getItem('accessToken');
+
 
 export default function CreateDestination() {
+  const navigate = useNavigate();
+
+  const createHandler = async (values) =>{
+    try {
+      const {_id: destinationId} = await createDest(values);
+      navigate(`/destination/${destinationId}/details`)
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+
+  const initialValues = {
+    title: '',
+    img: '',
+    details: '',
+  }
+
+  const {values, changeHandler, submitHandler} = useForm(initialValues, createHandler);
   return (
     <section className={styles['bg-img']}>
     <form
       className={styles["container"]}
+      onSubmit={submitHandler}
     >
       <div>
-        <h1>Създаване на дестинация</h1>
-        <p>Попълнете всички полета, за да добавите нова дестинация.</p>
+        <h1>Create Destination</h1>
+        <p>Fulfill all of the field, to add a new destination.</p>
         <hr />
 
         <label htmlFor="title">
-          <b>Заглавие</b>
+          <b>Title</b>
         </label>
         <input
           type="text"
-          placeholder="Enter Email"
+          placeholder="Enter Title"
           name="title"
           id="title"
+          value={values.title}
+          onChange={changeHandler}
         />
 
         <label htmlFor="image">
-          <b>Снимка</b>
+          <b>Picture</b>
         </label>
         <input
           type="text"
-          placeholder="Enter Password"
-          name="image"
+          placeholder="Place image"
+          name="img"
           id="image"
+          value={values.img}
+          onChange={changeHandler}
         />
 
         <label htmlFor="details">
-          <b>Детайли</b>
+          <b>Details</b>
         </label>
         <textarea
           type="text"
-          placeholder="Repeat Password"
+          placeholder="Enter Details"
           name="details"
           id="details"
+          value={values.details}
+          onChange={changeHandler}
         />
         <hr />
 
         <button type="submit" className={styles["registerbtn"]}>
-          Регистрация
+          Add
         </button>
-      </div>
-
-      <div className={styles[("container", "signin")]}>
-        <p>
-          Вече имате регистрация? <Link to="/login">Вход</Link>.
-        </p>
       </div>
     </form>
   </section>
--- client/src/hooks/useDestinations.js
+++ client/src/hooks/useDestinations.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { getAll, getOne } from "../api/dest-api";
+import { createDest, getAll, getOne } from "../api/dest-api";
 
 export function useGetAllDestinations() {
 
@@ -22,9 +22,16 @@ export function useGetOneDestination(destId) {
     useEffect(() => {
         (async () => {
             const result = await getOne(destId);
+            
             setDest(result[0]);
         })();
     }, [ destId ]);
 
     return [ dest ];
 };
+
+export function useCreateDestination() {
+    const destCreateHandler = (destData) => createDest(destData);
+
+    return destCreateHandler;
+}
