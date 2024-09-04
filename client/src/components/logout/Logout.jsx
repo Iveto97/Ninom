cf701320135587e0feaead87b4b8f0a17feccc3d
Iva <141308984+Iveto97@users.noreply.github.com>
2024-09-04 12:24:47 +0300

    Update Logout.jsx

--- client/src/components/logout/Logout.jsx
+++ client/src/components/logout/Logout.jsx
@@ -1,11 +1,9 @@
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate} from "react-router-dom";
 import { useLogout } from "../../hooks/useAuth";
-import useForm from "../../hooks/useForm";
 
 export default function Logout() {
     const logout = useLogout();
 
-   
             logout();
         return <Navigate to={'/'} />
 };
