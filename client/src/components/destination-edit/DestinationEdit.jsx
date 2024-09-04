6b66ef16e426923ec5e06e28035fa62f03a51dbf
Iva <141308984+Iveto97@users.noreply.github.com>
2024-09-04 12:18:30 +0300

    Update DestinationEdit.jsx

--- client/src/components/destination-edit/DestinationEdit.jsx
+++ client/src/components/destination-edit/DestinationEdit.jsx
@@ -1,46 +1,62 @@
 import { useNavigate, useParams } from "react-router-dom";
+
 import useForm from "../../hooks/useForm";
 import { useGetOneDestination } from "../../hooks/useDestinations";
+
 import { updateDest } from "../../api/dest-api";
+
 import styles from "../destination-edit/DestinationEdit.module.css";
 
-// const initialValues = {
-//     title: '',
-//     imageUrl: '',
-//     details: '',
-// }
-
 export default function DestinationEdit() {
-    const navigate = useNavigate();
-    const { destinationId } = useParams();
-    const [dest] = useGetOneDestination(destinationId);
-    
-    // const initialFormValues = useMemo(() => Object.assign({}, initialValues, dest), [dest])
+  const navigate = useNavigate();
+  const { destinationId } = useParams();
+  const [dest] = useGetOneDestination(destinationId);
 
-    const {changeHandler, submitHandler, values } = useForm(dest, async (values) => {
-        const updatedDest = await updateDest(destinationId, values);
-        
-        navigate(`/destination/${destinationId}/details`);
-    }, { reinitializeForm: true });
+  const { changeHandler, submitHandler, values } = useForm(
+    dest,
+    async (values) => {
+      const updatedDest = await updateDest(destinationId, values);
+      navigate(`/destination/${destinationId}/details`);
+    },
+    { reinitializeForm: true }
+  );
 
-    return (
-        <section id="edit-page" className="auth">
-            <form id="edit" onSubmit={submitHandler}>
-                <div className={styles["container"]}>
-
-                    <h1>Edit Destination</h1>
-                    <label htmlFor="title">Title:</label>
-                    <input type="text" id="title" name="title" onChange={changeHandler} value={values.title}/> {/*values.title || ''*/}
-
-                    <label htmlFor="game-img /">Image:</label>
-                    <input type="text" id="imageUrl" name="imageUrl" onChange={changeHandler} value={values.imageUrl}/>
-
-                    <label htmlFor="details">Details:</label>
-                    <textarea name="details" id="details" onChange={changeHandler} value={values.details}></textarea>
-                    <input className={styles["edit-btn"]} type="submit" value="Edit Destination" />
-
-                </div>
-            </form>
-        </section>
-    );
+  return (
+    <section id="edit-page" className="auth">
+      <form id="edit" onSubmit={submitHandler}>
+        <div className={styles["container"]}>
+          <h1>Edit Destination</h1>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            onChange={changeHandler}
+            value={values.title}
+          />{" "}
+          {/*values.title || ''*/}
+          <label htmlFor="game-img /">Image:</label>
+          <input
+            type="text"
+            id="imageUrl"
+            name="imageUrl"
+            onChange={changeHandler}
+            value={values.imageUrl}
+          />
+          <label htmlFor="details">Details:</label>
+          <textarea
+            name="details"
+            id="details"
+            onChange={changeHandler}
+            value={values.details}
+          ></textarea>
+          <input
+            className={styles["edit-btn"]}
+            type="submit"
+            value="Edit Destination"
+          />
+        </div>
+      </form>
+    </section>
+  );
 }
