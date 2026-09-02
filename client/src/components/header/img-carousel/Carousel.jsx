7993b8e916f19038be95f349daa954673000d9e2
Iva <iveto9244@gmail.com>
2026-09-02 10:01:40 +0300

    Change carousel images path

--- client/src/components/header/img-carousel/Carousel.jsx
+++ client/src/components/header/img-carousel/Carousel.jsx
@@ -1,11 +1,17 @@
 
-let img = ['Vratsa.jpg', 'Troyan Monastery.jpg', 'Tylenovo.jpg'];
+let img = [
+  '/images/Vratsa.jpg',
+  '/images/Troyan Monastery.jpg',
+  '/images/Tylenovo.jpg'
+];
 
 export default function ImgCarousel({ index }) {
 
+  const name = img[index].split('/').pop().split(".")[0].replace(/_/g, " ");
+  
     return (
         <div className="img-box">
-          <img src={`/public/images/${img[index]}`} alt="" />
+          <img src={img[index]} alt={`${name}`} />
         </div>
     );
   }
